660523dd65b608c78bdd6c5e305a044865b73b0b
jpe7s <jpe7s.salt188@passfwd.com>
2025-04-07 09:46:36 -0500

    re-add sanctum.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -20,4 +20,8 @@ module software.sava.solana_web2 {
   exports software.sava.solana.web2.jupiter.client.http;
   exports software.sava.solana.web2.jupiter.client.http.response;
   exports software.sava.solana.web2.jupiter.client.http.request;
+
+  exports software.sava.solana.web2.sanctum.client.http;
+  exports software.sava.solana.web2.sanctum.client.http.response;
+  exports software.sava.solana.web2.sanctum.client.http.request;
 }
